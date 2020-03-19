868bfed0dcaa725bfe9f03182ed3c352c10c00b4
CezSew <csewerynski@interia.pl>
2020-03-20 00:36:34 +0100

    Added styles for test hub

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,8 +1,8 @@
 import "./globals";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {RouterComponent} from "./router";
 import './css/utils/base.scss';
+import {RouterComponent} from "./router";
 import store from './store';
 import { Provider } from 'react-redux';
 
--- frontend/components/pages/TestsHub.tsx
+++ frontend/components/pages/TestsHub.tsx
@@ -27,17 +27,54 @@ class TestsHub extends React.Component <TestsHubProps> {
 
     render() {
         const {quizes_all_by_rating} = this.props;
+        console.log(quizes_all_by_rating)
         const quizes = Object.keys(quizes_all_by_rating).map((keyName, i) => {
-            return <li key={i}><b>{quizes_all_by_rating[keyName].title}</b></li>
+            return (
+            <li key={i} className="c-quiz-line">
+                <div className="c-quiz-line__short"><span className="c-quiz-line__circle">XD</span></div>
+                <div className="c-quiz-line__title">{quizes_all_by_rating[keyName].title}</div>
+                <div className="c-quiz-line__type">{quizes_all_by_rating[keyName].type}</div>
+                <div className="c-quiz-line__score">-</div>
+                <div className="c-quiz-line__rating">{quizes_all_by_rating[keyName].rating} ({quizes_all_by_rating[keyName].votes} głosów)</div>
+            </li>
+            )
         });
         return (
             <AuthOverlord>
                 <Header/>
                 <main className="c-test-hub">
                     <div className="o-container">
-                        <Link to="/create-quiz">Utwórz quiz</Link>
+                        <aside className="c-test-hub__menu">
+                            <Link to="/create-quiz" className="c-test-hub__menu-item">Utwórz quiz</Link>
+                        </aside>
+
+                        <section className="c-test-hub__main">
+                            <div className="c-test-hub__title-container">
+                                <h1 className="o-title o-title--h2 o-title--line c-test-hub__title">
+                                    Dostępne testy
+                                </h1>
+                            </div>
+                            <div className="c-test-hub__table">
+                                <div className="c-test-hub__table-header">
+                                    <div className="c-test-hub__table-th c-test-hub__table-th--name">
+                                        Nazwa quizu
+                                    </div>
+                                    <div className="c-test-hub__table-th c-test-hub__table-th--type">
+                                        Typ
+                                    </div>
+                                    <div className="c-test-hub__table-th c-test-hub__table-th--score">
+                                        Twój wynik
+                                    </div>
+                                    <div className="c-test-hub__table-th c-test-hub__table-th--rating">
+                                        Ocena
+                                    </div>
+                                </div>
+                                <ul className="c-test-hub__quiz-list">
+                                    {quizes}
+                                </ul>
+                            </div>
+                        </section>
                     </div>
-                    <ul>{quizes}</ul>
                 </main>
             </AuthOverlord>
         );
