9e1ded687db69d7337c98e00013ed091f1aee0d1
CezSew <csewerynski@interia.pl>
2020-05-26 20:00:59 +0200

    Added static content to creation wizard

--- frontend/components/pages/QuizCreator/representational/Dropfile.tsx
+++ frontend/components/pages/QuizCreator/representational/Dropfile.tsx
@@ -7,11 +7,11 @@ export const Dropfile = () => {
     }, [])
 
     return (
-        <section className="c-quiz-creator-form__dropfile-wrapper">
-            <div className="c-quiz-creator-form__dropfile-description-wrapper">
-                <p className="c-quiz-creator-form__dropfile-description o-underlined">Dodaj test z pliku tekstowego</p>
-                <p className="c-quiz-creator-form__dropfile-description">Test musi zawierać co najmniej 5 pytań. Struktura pliku powinna wyglądać następująco: pierwsza linia pliku jest pytaniem, następne cztery linie odczytaną zostaną jako odpowiedzi. Umieść plus przed poprawną odpowiedzią. Następne pytania nanoś analogicznie, od następnej linii następującej po czwartej odpowiedzi poprzedniego pytania.</p>
-                <p className="c-quiz-creator-form__dropfile-description">Pobierz plik z szablonem: <a href="#">Kliknij tutaj</a></p>
+        <section className="c-quiz-creator__dropfile-wrapper">
+            <div className="c-quiz-creator__dropfile-description-wrapper">
+                <p className="c-quiz-creator__description o-underlined">Dodaj test z pliku tekstowego</p>
+                <p className="c-quiz-creator__description">Test musi zawierać co najmniej 5 pytań. Struktura pliku powinna wyglądać następująco: pierwsza linia pliku jest pytaniem, następne cztery linie odczytaną zostaną jako odpowiedzi. Umieść plus przed poprawną odpowiedzią. Następne pytania nanoś analogicznie, od następnej linii następującej po czwartej odpowiedzi poprzedniego pytania.</p>
+                <p className="c-quiz-creator__description">Pobierz plik z szablonem: <a href="#">Kliknij tutaj</a></p>
             </div>
 
             <div className="c-quiz-creator-form__dropfile js-quiz-creator-form__dropfile">
--- frontend/components/pages/QuizCreator/representational/QuizWizard.tsx
+++ frontend/components/pages/QuizCreator/representational/QuizWizard.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { AddSVG } from "../../../utils/svg/Add";
 
 export const QuizWizard = () => {
     return (
-        <div className="c-quiz-creator__tool">
-            QuizWizard
+        <div className="c-quiz-creator-wizard">
+            <div className="c-quiz-creator-wizard__description-wrapper">
+                <p className="c-quiz-creator__description o-underlined">Dodaj test z użyciem kreatora</p>
+                <p className="c-quiz-creator__description">Test musi zawierać co najmniej 5 pytań. Kliknij przycisk "dodaj pytanie" aby wyświetlić pola pytania i odpowiedzi. Wypełnij pola i zaznacz poprawną odpowiedź po prawej stronie pola odpowiedzi. Tylko jedna odpowiedź może być prawidłowa. Dla poprawy czytelności listy możliwe jest zwijanie odpowiedzi do jednej linii, za pomocą strzałki po lewej stronie pytania.</p>
+            </div>
+            <button className="c-quiz-creator-wizard__add-button">
+                <span className="c-quiz-creator-wizard__add-text">dodaj pytanie</span>
+                <AddSVG color="#3B8BEB" classes="c-quiz-creator-wizard__add-icon"/>
+            </button>
         </div>
     )
 }
